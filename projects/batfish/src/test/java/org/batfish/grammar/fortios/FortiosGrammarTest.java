9ae37c387abb510019154a27ac8732a975e81dd0
Corina Miner <c.miner19@gmail.com>
2021-03-23 12:07:31 -0700

    remove unused variable

--- projects/batfish/src/test/java/org/batfish/grammar/fortios/FortiosGrammarTest.java
+++ projects/batfish/src/test/java/org/batfish/grammar/fortios/FortiosGrammarTest.java
@@ -534,7 +534,6 @@ public final class FortiosGrammarTest {
   @Test
   public void testBgpWarnings() throws IOException {
     String hostname = "bgp_warnings";
-    FortiosConfiguration vc = parseVendorConfig(hostname);
     Batfish batfish = getBatfishForConfigurationNames(hostname);
     Warnings parseWarnings =
         getOnlyElement(
